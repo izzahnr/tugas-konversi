55e4c365ddd43d085087a51c87687363a1531ab1
izza <faizzahgitulo@gmail.com>
2023-09-07 20:31:27 +0700

    add tugas

---
+++ tugas.py
@@ -0,0 +1,13 @@
+#mengonversi suhu celcius ke suhu fahrenhelt
+
+print("mengonversi suhu celcius ke suhu fahrenhelt")
+c=int(input("masukkan nilai celcius"))
+konversi = (c*9/5)+32
+print("hasil konversi suhu celcius ke suhu fahrenhelt",konversi)
+
+#mengonversi suhu fahrenhelt ke suhu celcius
+
+print("mengonversi suhu fahrenhelt")
+f=int(input("memasukkan nilai fahrenhelt"))
+konversi=(f-32)*5/9
+print("hasil konversi suhu fahrenhelt ke celcius",konversi)
